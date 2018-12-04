d5e05e03dd2978ee5dced2ef05fc443396ade5f4
Riccardo Crippa <therickys93@gmail.com>
2018-12-04 19:07:54 +0100

    primo commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void) {
+    //Stream sockets and rcv()
+
+    struct addrinfo hints, *res;
+    int sockfd;
+
+    char buf[4098];
+    int byte_count;
+
+    //get host info, make socket and connect it
+    memset(&hints, 0,sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    getaddrinfo("therickys93.altervista.org","80", &hints, &res);
+    sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    printf("Connecting...\n");
+    connect(sockfd,res->ai_addr,res->ai_addrlen);
+    printf("Connected!\n");
+    char *header = "GET /index.html HTTP/1.1\r\nHost: therickys93.altervista.org\r\n\r\n";
+    send(sockfd,header,strlen(header),0);
+    printf("GET Sent...\n");
+    //all right ! now that we're connected, we can receive some data!
+    byte_count = recv(sockfd,buf,sizeof(buf),0);
+    printf("recv()'d %d bytes of data in buf\n",byte_count);
+    printf("%.*s",byte_count,buf); // <-- give printf() the actual data size
+    return 0;
+}
